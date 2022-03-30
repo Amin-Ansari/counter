8c6038d6a3e9d4f65e6bbd1e5c03ccff03d3a8f8
Amin Ansari <devansari@yahoo.com>
2022-03-30 11:39:26 +0430

    The color changer method became better

--- js/index.js
+++ js/index.js
@@ -4,15 +4,35 @@ const resetButton = document.querySelector("#reset");
 var theNumber = document.querySelector("p");
 var currentValue = Number(theNumber.innerHTML);
 
+// the function which sets the numbers color according to their value
 function setTheColor() {
   if (theNumber.innerHTML > 0) {
-    document.getElementById("number").style.color = "green";
+    if (theNumber.classList.contains("zero-color")) {
+      theNumber.classList.remove("zero-color");
+    }
+    if (theNumber.classList.contains("nagative-color")) {
+      theNumber.classList.remove("nagative-color");
+    }
+    theNumber.classList.add("positive-color");
   } else if (theNumber.innerHTML < 0) {
-    document.getElementById("number").style.color = "red";
+    if (theNumber.classList.contains("zero-color")) {
+      theNumber.classList.remove("zero-color");
+    }
+    if (theNumber.classList.contains("positive-color")) {
+      theNumber.classList.remove("positive-color");
+    }
+    theNumber.classList.add("nagative-color");
   } else if (theNumber.innerHTML == 0) {
-    document.getElementById("number").style.color = "white";
+    if (theNumber.classList.contains("positive-color")) {
+      theNumber.classList.remove("nagative-color");
+    }
+    if (theNumber.classList.contains("nagative-color")) {
+      theNumber.classList.remove("nagative-color");
+    }
+    theNumber.classList.add("zero-color");
   }
 }
+
 function increaseOne() {
   currentValue++;
   theNumber.innerHTML = currentValue;
